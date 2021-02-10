2a584959051e4c3a9ba6040dfa5941ffc9f58791
AgostonAttila <atesz014@gmail.com>
2021-02-10 10:33:17 +0100

    move folders

---
+++ Reactivities/API/Startup.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Activities;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Persistence;
+
+namespace API
+{
+   public class Startup
+   {
+      public Startup(IConfiguration configuration)
+      {
+         Configuration = configuration;
+      }
+
+      public IConfiguration Configuration { get; }
+
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+
+         services.AddDbContext<DataContext>(opt =>
+         {
+            opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+         });
+
+         /*   services.AddCors(opt =>
+             {
+                opt.AddPolicy("CorsPolicy", policy =>
+                  {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowCredentials();
+                 });
+             });*/
+
+         services.AddCors(options =>
+         {
+            options.AddPolicy(name: "CorsPolicy",
+                       builder =>
+                       {
+                          builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowCredentials();
+                       });
+         });
+
+
+         services.AddMediatR(typeof(List.Handler).Assembly);
+         services.AddControllers()
+         .AddFluentValidation(cfg =>
+         {
+            cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+         });
+
+         services.AddSwaggerGen(c =>
+         {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+         });
+      }
+
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         if (env.IsDevelopment())
+         {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+         }
+
+         app.UseHttpsRedirection();
+
+         app.UseRouting();
+
+         app.UseAuthorization();
+
+         app.UseCors("CorsPolicy");
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapControllers();
+         });
+      }
+   }
+}
