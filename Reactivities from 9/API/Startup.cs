478030cee454658f0fb4f1c78a3240b5ed99d973
AgostonAttila <atesz014@gmail.com>
2021-02-24 18:37:01 +0100

    add Identity

--- Reactivities from 9/API/Startup.cs
+++ Reactivities from 9/API/Startup.cs
@@ -9,10 +9,12 @@ using Application.Core;
 using AutoMapper;
 using FluentValidation.AspNetCore;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,47 +25,54 @@ using Persistence;
 
 namespace API
 {
-    public class Startup
-    {
-        private readonly IConfiguration _config;
-        public Startup(IConfiguration config)
-        {
-            _config = config;
-        }
+   public class Startup
+   {
+      private readonly IConfiguration _config;
+      public Startup(IConfiguration config)
+      {
+         _config = config;
+      }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers().AddFluentValidation(config => 
-            {
-                config.RegisterValidatorsFromAssemblyContaining<Create>();
-            });
-            services.AddApplicationServices(_config);
-        }
+      // This method gets called by the runtime. Use this method to add services to the container.
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddControllers(opt =>
+         {
+            var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            opt.Filters.Add(new AuthorizeFilter(policy));
+         })
+            .AddFluentValidation(config =>
+         {
+            config.RegisterValidatorsFromAssemblyContaining<Create>();
+         });
+         services.AddApplicationServices(_config);
+         services.AddIdentityServices(_config);
+      }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            app.UseMiddleware<ExceptionMiddleware>();
+      // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+      {
+         app.UseMiddleware<ExceptionMiddleware>();
 
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+         if (env.IsDevelopment())
+         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+         }
 
-            // app.UseHttpsRedirection();
+         // app.UseHttpsRedirection();
 
-            app.UseRouting();
+         app.UseRouting();
 
-            app.UseCors("CorsPolicy");
+         app.UseCors("CorsPolicy");
 
-            app.UseAuthorization();
+         app.UseAuthentication();
+         app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+         app.UseEndpoints(endpoints =>
+         {
+            endpoints.MapControllers();
+         });
+      }
+   }
 }
