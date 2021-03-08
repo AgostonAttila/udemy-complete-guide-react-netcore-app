8de4a4ca8621abfe2cf81cc1ab29b3cf69362721
AgostonAttila <atesz014@gmail.com>
2021-03-08 14:44:12 +0100

    add Photo upload client side

--- Reactivities from 9/client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ Reactivities from 9/client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -16,12 +16,14 @@ export default function ActivityListItem({ activity }: Props) {
         {activity.isCancelled && <Label attached="top" color="red" content="Cancelled" style={{ textAlign: 'center' }} />}
         <Item.Group>
           <Item>
-            <Item.Image style={{ marginBottom: 3 }} size="tiny" circular src="/assets/user.png" />
+            <Item.Image style={{ marginBottom: 3 }} size="tiny" circular src={activity.host?.image || '/assets/user.png'} />
             <Item.Content>
               <Item.Header as={Link} to={`/activities/${activity.id}`}>
                 {activity.title}
               </Item.Header>
-              <Item.Description>Hosted by {activity.host?.displayName}</Item.Description>
+              <Item.Description>
+                Hosted by <Link to={`/profiles/${activity.hostUsername}`}> {activity.host?.displayName}</Link>
+              </Item.Description>
               {activity.isHost && (
                 <Item.Description>
                   <Label basic color="orange">
