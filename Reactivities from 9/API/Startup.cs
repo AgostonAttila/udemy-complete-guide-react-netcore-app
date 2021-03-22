a237842e851c3aa0f42fcf11cbf31a081c14aaf0
atesz <atesz014@gmail.com>
2021-03-22 20:12:56 +0100

    Security headers

--- Reactivities from 9/API/Startup.cs
+++ Reactivities from 9/API/Startup.cs
@@ -55,10 +55,33 @@ namespace API
         {
             app.UseMiddleware<ExceptionMiddleware>();
 
+            app.UseXContentTypeOptions();
+            app.UseReferrerPolicy(opt => opt.NoReferrer());//
+            app.UseXXssProtection(opt => opt.EnabledWithBlockMode());//cross site scripting
+            app.UseXfo(opt => opt.Deny());//not use as iframe
+            app.UseCsp(opt => opt  //content security policy
+                .BlockAllMixedContent()
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+                .FormActions(s => s.Self())
+                .FrameAncestors(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-3xI/RwSu97oTc/z0YwU7hP7sGn7xhd8/kS+brVdThMM="))
+            );
+
+
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+            }
+            else
+            {
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=315");
+                    await next.Invoke();
+                });
             }
 
             // app.UseHttpsRedirection();
