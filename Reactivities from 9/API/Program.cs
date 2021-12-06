6fcdd9e3998c16f701fe764a4c43288d30129f39
atesz014 <atesz014@citromail.hu>
2021-12-06 21:13:42 +0100

    before dotnet 6

--- Reactivities from 9/API/Program.cs
+++ Reactivities from 9/API/Program.cs
@@ -1,3 +1,32 @@
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+*/
+
 using System;
 using System.Threading.Tasks;
 using Domain;
